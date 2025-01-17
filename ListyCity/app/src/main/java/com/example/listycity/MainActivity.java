1630f6fcaa5291ab8f846ececa24dced981be42f
jayeshkumargoyal <kumarjayesh012@gmail.com>
2025-01-16 18:04:01 -0700

    Lab 2 Participation Exercise

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
     // defining attributes of main activity class
     ListView cityList;
+    EditText inputtext1;
+    Button btnAdd, btnUpdate;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    Integer indexVal;
+    String item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         cityList = findViewById(R.id.city_list);
+        btnAdd = findViewById(R.id.button_add);
+        //btnUpdate = findViewById(R.id.button_up);
+        inputtext1 = findViewById(R.id.editText);
+
 
         String[] cities = {"Edmonton", "Paris", "London"};
 
@@ -44,6 +57,47 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        // Adding a new Item
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringval = inputtext1.getText().toString();
+                dataList.add(stringval);
+                cityAdapter.notifyDataSetChanged();
+                inputtext1.setText("");
+            }
+        });
 
+        // Selecting a new Item
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                item = adapterView.getItemAtPosition(i).toString() + " has been selected";
+                indexVal = i;
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        // Updating the City Information
+//        btnUpdate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String stringval = inputtext1.getText().toString();
+//                dataList.set(indexVal, stringval);
+//                cityAdapter.notifyDataSetChanged();
+//            }
+//        });
+
+        // Deleting a city
+        cityList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                item = adapterView.getItemAtPosition(i).toString() + " has been deleted";
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+                dataList.remove(i);
+                cityAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 }
